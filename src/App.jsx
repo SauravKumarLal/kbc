f0e0bc408f2027b1f42d5a8af99c058a35038546
SauravKumarLal <sauravkrlal@gmail.com>
2024-06-14 13:46:10 +0530

    KBC app doing..

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main">main</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem">₹ 40,000</li>
+          <li className="moneyListItem">₹ 20,000</li>
+          <li className="moneyListItem">₹ 10,000</li>
+          <li className="moneyListItem">₹ 5,000</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
